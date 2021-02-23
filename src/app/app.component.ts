c810ad58763aa974efaef4c2d2acd2a03d42489b
vitolaman <vito99varianlaman@gmail.com>
2021-02-23 19:59:53 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,43 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  turn = -1;
+  inp = ""
+  a = 0;
+  b = 0;
+  tek = 0
+  items = [
+    ["*","*","*","*","*"],
+    ["*","*","*","*","*"],
+    ["*","*","*","*","*"],
+    ["*","*","*","*","*"],
+    ["*","*","*","*","*"]
+  ];
+
+JALAN(a,b) {
+  var ca = this.a;
+  var cb = this.b;
+  var cek = 0;
+  if(ca && cb < 6 && ca && cb > 0){
+    if(this.items[ca-1][cb-1] == "*"){
+      this.turn +=1;
+      this.inp = String(this.turn%2);
+      this.items[this.a-1][this.b-1] = this.inp;
+
+      for (var i = 0; i < 5; i++) {
+        for (var j = 0; j < 5; j++) {
+          console.log("tes");
+        }
+      }
+
+    } else{
+      alert("sudah terisi!");
+    }
+
+  } else{
+    alert("koordinat salah");
+  }
+
+}
+
 }
